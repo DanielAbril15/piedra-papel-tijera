ff7aa45dbac29cb9600a3252f81a2036d28a1733
Daniel Abril <danbril1015@gmail.com>
2022-07-05 12:47:53 -0500

    Agrego funcionalidades de botones y resultado

--- code.js
+++ code.js
@@ -1,36 +1,65 @@
-const userRock = document.getElementById("rock");
-const userPaper = document.getElementById("paper");
-const userScissors = document.getElementById("scissors");
-const startGame = document.getElementById("action");
-const resultGame = document.getElementById("result");
+const startGame = document.querySelector("#action");
+const buttonGame = document.querySelector(".tool-container");
+const selectionGame = document.querySelector("#selection");
+const userSelection = document.querySelector(".user_selection");
+const cpuSelection = document.querySelector(".cpu_selection");
+const resultGame = document.querySelector(".result");
 
 let userOption;
 let cpuOption;
 // Numero random de 1 a 3 para la eleccion de la cpu
-const cpuRandom = function random() {
+function cpuRandom() {
   return Math.floor(Math.random() * 3 + 1);
-};
+}
 
-// eleccion de jugador transformado a dato numerico
-userRock.onclick = function () {
-  return (userOption = 1);
-};
-userPaper.onclick = function () {
-  return (userOption = 2);
-};
-userScissors.onclick = function () {
-  return (userOption = 3);
-};
+// eleccion de jugador transformado a dato numerico y visualizacion en pantalla de la eleccion
+document.addEventListener("click", function (event) {
+  if (event.target.dataset.id === "rock") {
+    userOption = 1;
+    cpuOption = cpuRandom();
+    userSelection.innerHTML = `<div id="user-selection" class="selection-game icon-piedra">`;
+  } else if (event.target.dataset.id === "paper") {
+    userOption = 2;
+    cpuOption = cpuRandom();
+    userSelection.innerHTML = `<div id="user-selection" class="selection-game icon-papel">`;
+  } else if (event.target.dataset.id === "scissors") {
+    userOption = 3;
+    cpuOption = cpuRandom();
+    userSelection.innerHTML = `<div id="user-selection" class="selection-game icon-tijera">`;
+  }
+});
 
 // Escenarios en los que el usuario gana
 function optionsWin(userOp, cpuOp) {
   if (userOp === 1 && cpuOp === 3) {
-    return true;
+    return (resultGame.innerHTML = `<p class='win'>¡¡¡GANASTE!!!</p>`);
   }
   if (userOp === 2 && cpuOp === 1) {
-    return true;
+    return (resultGame.innerHTML = `<p class='win'>¡¡¡GANASTE!!!</p>`);
   }
   if (userOp === 3 && cpuOp === 2) {
-    return true;
+    return (resultGame.innerHTML = `<p class='win'>¡¡¡GANASTE!!!</p>`);
+  }
+  if (userOp === cpuOp) {
+    resultGame.innerHTML = "<p class ='draw'>Empate</p>";
+  } else {
+    resultGame.innerHTML = `<p class='lose'>Perdiste :'(</p>`;
   }
 }
+
+// dessarrollo del juego
+
+startGame.addEventListener("click", function (event) {
+  userOption === undefined
+    ? alert("Tienes que seleccionar una opcion para empezar el juego!")
+    : (cpuOption = cpuRandom());
+
+  if (cpuOption === 1) {
+    cpuSelection.innerHTML = `<div id="cpu-selection" class="selection-game icon-piedra">`;
+  } else if (cpuOption === 2) {
+    cpuSelection.innerHTML = `<div id="cpu-selection" class="selection-game icon-papel">`;
+  } else if (cpuOption === 3) {
+    cpuSelection.innerHTML = `<div id="cpu-selection" class="selection-game icon-tijera">`;
+  }
+  optionsWin(userOption, cpuOption);
+});
